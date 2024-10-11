153e2e49788b3755df1c9fadf7c39de44675f0b7
JulienMerdinger <julien.merdinger@gmail.com>
2024-10-11 15:43:45 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ def carre(val_user):
 def somme(val1, val2):
     return "<h2>La somme de votre valeur est : </h2>" + str(val1 + val2)
 
-@app.route('/parite/<int:val>')
+@app.route('/pair/<int:val>')
 def pair(val):
   if val % 2 == 0:
     return str(val) + "<p>est pair.</p>"
